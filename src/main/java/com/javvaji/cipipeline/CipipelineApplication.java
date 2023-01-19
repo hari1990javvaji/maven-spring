2af3acf7950649754039bba0be05672921b628bc
student <hkishore42@gmail.com>
2023-01-19 13:36:17 +0000

    spring code from start.spring.io

---
+++ src/main/java/com/javvaji/cipipeline/CipipelineApplication.java
@@ -0,0 +1,13 @@
+package com.javvaji.cipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipipelineApplication.class, args);
+	}
+
+}
